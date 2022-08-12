1158054166d9bdb1261d51d5a13b7d72929c9890
Paul-cmd29 <paulpetrov8510@gmail.com>
2022-08-12 09:39:10 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+public class Lesson
+{
+    static void Main(string[] args)
+    {
+        Sort();
+        
+
+
+    }
+
+
+    //public enum orderBy
+    //{
+    //    Asc = + 1,
+    //    Desc = - 1,
+
+
+    //}
+
+
+    public static void Sort()
+    {
+        sortAlgoritmTypes a = new();
+        int[] array = new int[10] { 56, 1, 99, 67, 89, 23, 44, 12, 78, 34 };
+        a.selectionSort(array);
+        a.bubbleSort(array);
+        a.printArray(array);
+        a.insertionSort(array);
+        //orderBy = orderBy.Asc | orderBy.Desc;
+        //Console.WriteLine(orderBy);
+
+        
+
+
+    }
+   
+    public class sortAlgoritmTypes
+    {
+        public void selectionSort(int[] array)
+        {
+            //Selection sort
+
+            int n = 10;
+            Console.WriteLine("Selection sort");
+            Console.Write("Initial array is: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            int temp, smallest;
+            for (int i = 0; i < n - 1; i++)
+            {
+                smallest = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (array[j] < array[smallest])
+                    {
+                        smallest = j;
+                    }
+                }
+                temp = array[smallest];
+                array[smallest] = array[i];
+                array[i] = temp;
+            }
+            Console.WriteLine();
+            Console.Write("Sorted selection array is: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+        // bubble sort
+        public void bubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (arr[j] > arr[j + 1])
+                    {
+                        
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+        }
+
+        
+        public void printArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+        // insertion sort
+        public void insertionSort(int[] array_1)
+        {
+            int n = array_1.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = array_1[i];
+                int j = i - 1;
+
+
+                while (j >= 0 && array_1[j] > key)
+                {
+                    array_1[j + 1] = array_1[j];
+                    j -= 1;
+                }
+                array_1[j + 1] = key;
+            }
+
+            n = array_1.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(array_1[i] + " ");
+
+            Console.Write("\n");
+
+        }
+
+    }
+
+    
+
+    
+
+}
+
+
+
